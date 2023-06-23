8843ba8557e283171e76e558d0f2a62e668912b3
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-06-23 10:32:23 -0300

    instalação inicial do projeto MemoryGame

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+export function Card (){
+    return (
+    <div className="card">
+        <div className="card__content">
+            <div className="card__face">?</div>
+            <div className="card__face">🐍</div>
+        </div>
+    </div>
+    );
+}
---
+++ src/templates/App/index.tsx
@@ -0,0 +1,7 @@
+import { Card } from '../../components/Card';
+import './style.css';
+
+export function App() {
+  return <Card />;
+}
+
